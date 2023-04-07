165cd7c94ec1d8f10f0a26f251700265cc818a3b
Marko Cuk <marko.cuk.brains22@gmail.com>
2023-04-07 14:01:25 +0200

    friday update

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validateResourse from "./middleware/valivateResourse";
+import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200)); 
-     
+
+    app.post("/api/users", validateResourse(createUserSchema), createUserHandler); // this way request not being validated at all
+                                                                // need to add middleware between path and handler to validate handler
 };
 
 export default routes;
---
+++ src/schema/user.schema.ts
@@ -0,0 +1,24 @@
+// this schema will be used for all of our users endpoints
+import { object, string, TypeOf } from "zod";
+
+export const createUserSchema = object({ // definition for payload
+    body: object({                       // body equals to another object
+        name: string({
+            required_error: "Name is required"
+        }),
+        password: string({
+            required_error: "Password is required"
+        }).min(6, "Password too short - should be 6 chars minimum"),
+    passwordConfirmation: string({
+            required_error: "PasswordConfirmation is required"
+        }),
+        email: string({
+            required_error: "Email is required",
+        }).email("Not a valid email")
+    }).refine((data) => data.password === data.passwordConfirmation, { //callback with argument named data
+        message: "Password do not match",
+        path: ["passwordConfirmation"],
+    }),
+});
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>;
