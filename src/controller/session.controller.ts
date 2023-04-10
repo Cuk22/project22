360b4a5a77ceb42918a651ce8363e263a71b48d6
Marko Cuk <marko.cuk.brains22@gmail.com>
2023-04-10 09:18:50 +0200

    removed pass

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -3,4 +3,9 @@ import { Request, Response } from "express";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
     
+    // Validate users password (create a service in users service for validating users password)
+    // create a session
+    // create an access token
+    // create a refresh token
+    // return access & refresh token
 }
